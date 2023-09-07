1e3ebc248f5480a43f659e4398c54d0639353c22
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2023-09-08 00:46:42 +0100

    Hello, puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+include <stdio.h>
+/**
+* main - Usng printf function to print 
+* Return(0)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n"
+retun  (0);
+
