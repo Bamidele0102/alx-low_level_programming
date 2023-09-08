6dc83ba66f5d634074e8c68a862a3a8e25773081
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2023-09-08 01:27:04 +0100

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Usng printf function to print 
- * Return: 0 (Success)
- */
+* main - Usng printf function to print
+* Return: 0 (Success)
+*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
