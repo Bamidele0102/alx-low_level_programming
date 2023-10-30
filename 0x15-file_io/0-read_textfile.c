70cf92a1c4030a40caac8cf219e24174c0dc4744
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2023-10-30 14:49:57 +0100

    Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * read_textfile - Reads and prints a text file to the POSIX standard output.
+ * @filename: The name of the text file to read.
+ * @letters: The number of letters (characters) to read and print.
+ *
+ * Return: On success, returns the actual number of letters read and printed.
+ *         On failure, or if the file cannot be opened or read, returns 0.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, bytes_read, bytes_written;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(letters);
+	if (buffer == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+	return (bytes_written);
+}
+
