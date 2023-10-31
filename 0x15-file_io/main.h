239f4262c3d8edb66982472d02c8f407e9edb0b2
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2023-10-31 03:15:24 +0100

    elf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -18,10 +18,4 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int copy_file(const char *file_from, const char *file_to);
-int display_elf_header(const char *elf_filename);
-int open_source_file(const char *file_from);
-int create_or_open_destination_file(const char *file_to);
-void copy_data(int fd_from, int fd_to);
-
 #endif
