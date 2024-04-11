38c414cb98987245a9f4f42eec0d7a11f8ff326a
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-04-11 22:57:44 +0100

    Advanced binary search

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -12,33 +12,33 @@
  */
 int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
 {
-    size_t mid, i;
+	size_t mid, i;
 
-    if (left > right)
-        return (-1);
+	if (left > right)
+		return (-1);
 
-    mid = left + (right - left) / 2;
+	mid = left + (right - left) / 2;
 
-    printf("Searching in array: ");
-    for (i = left; i <= right; i++)
-    {
-        printf("%d", array[i]);
-        if (i < right)
-            printf(", ");
-    }
-    printf("\n");
+	printf("Searching in array: ");
+	for (i = left; i <= right; i++)
+	{
+		printf("%d", array[i]);
+		if (i < right)
+			printf(", ");
+	}
+	printf("\n");
 
-    if (array[mid] == value)
-    {
-        if (mid == left || array[mid - 1] != value)
-            return (mid);
-        else
-            return (advanced_binary_recursive(array, left, mid, value));
-    }
-    else if (array[mid] < value)
-        return (advanced_binary_recursive(array, mid + 1, right, value));
-    else
-        return (advanced_binary_recursive(array, left, mid - 1, value));
+	if (array[mid] == value)
+	{
+		if (mid == left || array[mid - 1] != value)
+			return (mid);
+		else
+			return (advanced_binary_recursive(array, left, mid, value));
+	}
+	else if (array[mid] < value)
+		return (advanced_binary_recursive(array, mid + 1, right, value));
+	else
+		return (advanced_binary_recursive(array, left, mid - 1, value));
 }
 
 /**
@@ -52,8 +52,8 @@ int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-    if (array == NULL || size == 0)
-        return (-1);
+	if (array == NULL || size == 0)
+		return (-1);
 
-    return (advanced_binary_recursive(array, 0, size - 1, value));
+	return (advanced_binary_recursive(array, 0, size - 1, value));
 }
